8f2f750cf7909cf40f2448c5510aa04762c751fc
Iago Carvalho <iagobgc@gmail.com>
2025-05-08 17:14:38 -0300

    Questionário 05 - Final

--- Questionário 05 - Strings/Questao09.py
+++ Questionário 05 - Strings/Questao09.py
@@ -6,28 +6,32 @@ def reverse(s):
 
 def decodifica(codigoBinario, codigoUnario):
     n = len(codigoUnario)
-    if n % 2 == 0:
+    if n % 2 == 1:  
         zero = codigoBinario[n-1]
-        um = [c for c in codigoBinario if c != zero][0]
-    else:
+        for c in codigoBinario:
+            if c != zero:
+                um = c
+                break
+    else:         
         um = codigoBinario[n-1]
-        zero = [c for c in codigoBinario if c != um][0]
-    
+        for c in codigoBinario:
+            if c != um:
+                zero = c
+                break
     return zero, um
 
 def converte2alien(zero, um, code):
     binario = decimal2bin(int(code))
-    return "".join(um if bit == "1" else zero for bit in binario)
+    return "".join(zero if bit == "0" else um for bit in binario)
 
 def converte2decimal(zero, um, code):
-    binario = "".join("1" if c == um else "0" for c in code)
+    binario = "".join("0" if c == zero else "1" for c in code)
     return int(binario, 2)
 
 def isBigEndian(codigoBinario, codigoUnario):
     n = len(codigoUnario)
     zero, um = decodifica(codigoBinario, codigoUnario)
-    
-    binario = "".join("1" if c == um else "0" for c in codigoBinario)
+    binario = "".join("0" if c == zero else "1" for c in codigoBinario)
     return int(binario, 2) != n
 
 def BigEndian2LittleEndian(codigoBinario):
@@ -42,11 +46,11 @@ n = len(codigoUnario)
 bigEndian = isBigEndian(codigoBinario, codigoUnario)
 if bigEndian:
     codigoBinario = BigEndian2LittleEndian(codigoBinario)
-
+    
 zero, um = decodifica(codigoBinario, codigoUnario)
 
 for _ in range(n):
-    code = input()
+    code = input().strip()
     if code.isdigit():
         resultado = converte2alien(zero, um, code)
         if bigEndian:
