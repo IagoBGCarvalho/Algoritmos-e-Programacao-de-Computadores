df644156edd2e28ef92bdac2d7921a3e6686c3f6
Iago Carvalho <iagobgc@gmail.com>
2025-06-02 17:44:21 -0300

    Questionário 07 - Parte 1

---
+++ Questionário 07 - Funções/questao01.py
@@ -0,0 +1,7 @@
+def fahrenheit(graus_celsius):
+    celsius_para_fahrenheit = 1.8 * graus_celsius + 32
+    print(f"Fahrenheit: {celsius_para_fahrenheit:.2f}")
+
+def kelvin(graus_celsius):
+    celsius_para_kelvin = graus_celsius + 273.15
+    print(f"Kelvin: {celsius_para_kelvin:.2f}")
